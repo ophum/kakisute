b3f0f392d68a0d8fd606fc82ce26925a12afc39d
ophum <inagaki0106@gmail.com>
2025-10-13 03:02:43 +0900

    PROMETHEUS_MULTIPROC_DIRを指定するとdjango-prometheusでmultiprocessの設定をしてくれるので不要

--- django-wsgi-prom-test/gunicorn_settings.py
+++ django-wsgi-prom-test/gunicorn_settings.py
@@ -1,11 +1,3 @@
-#from prometheus_client import CollectorRegistry, multiprocess
-#
-#def when_ready(_):
-#    multiprocess.MultiProcessCollector(CollectorRegistry())
-#
-#def child_exit(_, worker):
-#    multiprocess.mark_process_dead(worker.pid)
-
 wsgi_app = 'prom_test.wsgi:application'
 
 daemon = False
